92fc2815dfb41c108f8b24e9ac56ef032900d1c2
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-06 11:38:01 -0800

    Fresh start to repo

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+const PORT = 3003;
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+  console.log(`listening on PORT ${PORT}~!?`);
 });
