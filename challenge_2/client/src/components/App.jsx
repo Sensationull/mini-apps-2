6ab6ae39a2892034f0327f354d8ab8b199268098
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-06 13:55:35 -0800

    added API request to server, receiving historical data to client

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -3,9 +3,29 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      chartInfo: '',
+    };
+    this.generateChart = this.generateChart.bind(this);
+  }
+
+  generateChart() {
+    fetch('/current')
+      .then(response => response.json())
+      .then((data) =>{
+        this.setState({
+          chartInfo: data,
+        });
+      })
+      .catch(() => console.log('Error'));
+  }
+
+  componentDidMount() {
+    this.generateChart();
   }
 
   render() {
+    (console.log('this is the chart Info', this.state.chartInfo.bpi));
     return (
       <div>Hello</div>
     );
--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/current', (req, res) => {
-  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
     .then(response => response.json())
     .then((data) => {
       res.send(data);
