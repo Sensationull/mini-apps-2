c3db5cf9a8cf93f854716fb79ef6b455f90fa544
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-06 12:02:51 -0800

    added fetch capability on server, and required packages

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -3,11 +3,21 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 3003;
+const fetch = require('node-fetch');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/current', (req, res) => {
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => response.json())
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => { console.log(error); });
+});
 
 app.listen(PORT, function() {
   console.log(`listening on PORT ${PORT}~!?`);
