e8c7e498b816e321ffdc0ee175a49be477f407fb
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-07 13:40:59 -0800

    styling and code to pick pins

--- challenge_3/client/src/components/App.jsx
+++ challenge_3/client/src/components/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import Pins from './Pins.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      score: ''
+    };
+    this.pinClick = this.pinClick.bind(this);
+  }
+
+  pinClick (e) {
+    var pin = e.target.innerText;
+    console.log('pinclick state', pin);
+    this.setState({
+      score: pin,
+    }); 
   }
 
   render() {
     return (
-      <div>THE WORLD IS WAITING</div>
+      <div>THE WORLD IS WAITING
+        <Pins score={this.pinClick}></Pins>
+      </div>
     );
   }
 }
---
+++ challenge_3/client/src/components/Pins.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Pins = (props) => (
+  <div className="pins" onClick={props.score}>
+    <div className="pin">1</div>
+    <div className="pin">2</div>
+    <div className="pin">3</div>
+    <div className="pin">4</div>
+    <div className="pin">5</div>
+    <div className="pin">6</div>
+    <div className="pin">7</div>
+    <div className="pin">8</div>
+    <div className="pin">9</div>
+    <div className="pin">10</div>
+  </div>
+);
+
+export default Pins;
