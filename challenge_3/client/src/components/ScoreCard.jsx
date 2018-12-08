9db335b180216d231a84e72fbbb546e9f010f1ff
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-07 21:26:46 -0800

    updated gitignore, total score now also represented

--- challenge_3/client/src/components/ScoreCard.jsx
+++ challenge_3/client/src/components/ScoreCard.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const ScoreCard = (props) => (
   <div>
-    {props.bowl.map(function(item, index) {  
-      return <div key={index}>{item}</div>;
-    })}
+    <div>Total Score: {props.bowl[props.bowl.length - 1]}
+      {props.bowl.map(function(item, index) {  
+        return <div key={index}>{item}</div>;
+      })}
+    </div>
   </div>
 );
 
